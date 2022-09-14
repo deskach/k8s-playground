f044247f0f94faacb5bc20b3c23d00422400f68b
deskach <deskach@gmail.com>
2022-09-14 15:22:46 +0300

    Add express-validator middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import {body, validationResult} from "express-validator";
 
 const router = express.Router();
 
-router.get("/api/users/signup", (req: any, res: any) => {
-    res.post("signup");
-})
+router.post("/api/users/signup",
+    [
+        body('email').isEmail().withMessage('A valid email is required'),
+        body('password')
+            .trim()
+            .isLength({min: 4, max: 20})
+            .withMessage('Password must be from 4 to 20 chars'),
+    ], // ^ middlewares
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
+
+        // const {email, password} = req.body;
+
+        console.log("Creating a user");
+
+        res.send("signup");
+    }
+)
 
 export {router as signUpRouter};

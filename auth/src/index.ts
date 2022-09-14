8abc0c8e4b920c62d54de558ed9dc60ccb924e46
deskach <deskach@gmail.com>
2022-09-14 17:39:36 +0300

    Lay the ground for error-handling express middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import {currentUserRouter} from './routes/currentUser';
 import {signInRouter} from "./routes/signIn";
 import {signOutRouter} from "./routes/signout";
 import {signUpRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
 
 const PORT = 3000;
 const app = express();
@@ -13,5 +14,6 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

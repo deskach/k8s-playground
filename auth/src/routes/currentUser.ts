3997dfbfb0c52e9ce02bbb0c5aa4a92b0bf3560e
deskach <deskach@gmail.com>
2022-09-29 17:24:36 +0300

    Implement current-user route

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import {isEmpty} from "../util";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/current-user", (req: any, res: any) => {
-    res.send("Hello world!\n");
+router.get("/api/users/current-user", (req: Request, res: Response) => {
+    const EMPTY_USER = {currentUser: null};
+    const token = req?.session?.jwt;
+
+    if (isEmpty(token)) {
+        return res.send(EMPTY_USER);
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_KEY!);
+
+        return res.send(payload);
+    } catch (err: any) {
+        return res.send(EMPTY_USER);
+    }
 })
 
 export {router as currentUserRouter};
